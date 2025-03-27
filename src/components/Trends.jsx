987c6b562f292a3d3b938aff444c840c14be36d0
heywowyou <pro@julesfontaine.com>
2025-03-27 19:23:43 +0100

    Get trends title outside of hashtags box

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -21,29 +21,29 @@ export default function Trends({ refresh }) {
   }, [refresh]);
 
   return (
-    <div className="bg-[#202C38] p-8 rounded-xl shadow-md text-white w-full">
-      <h2 className="text-2xl font-bold mb-3 tracking-wide">Trends</h2>
-      <ul className="space-y-4">
-        {trends.map((tag) => {
-          const name = tag.hashtag.replace(/^#/, "");
-          return (
-            <li
-              key={tag.hashtag}
-              className="cursor-pointer hover:underline"
-              onClick={() => router.push(`/hashtag/${name}`)}
-            >
-              <div className="flex flex-col gap-y-2">
-                <span className="text-white font-bold text-xl tracking-wide">
-                  #{name}
-                </span>
-                <span className="text-gray-400 text-sm tracking-wide">
-                  {tag.count} tweet{tag.count > 1 ? "s" : ""}
-                </span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="w-full">
+      <h2 className="text-3xl font-bold m-4 ml-2 text-white">Trends</h2>
+      <div className="bg-[#202C38] p-6 pl-8 rounded-xl shadow-md text-white w-full">
+        <ul className="space-y-4">
+          {trends.map((tag) => {
+            const name = tag.hashtag.replace(/^#/, "");
+            return (
+              <li
+                key={tag.hashtag}
+                className="cursor-pointer hover:underline"
+                onClick={() => router.push(`/hashtag/${name}`)}
+              >
+                <div className="flex flex-col gap-y-2">
+                  <span className="text-white font-bold text-xl">#{name}</span>
+                  <span className="text-gray-400 text-sm">
+                    {tag.count} tweet{tag.count > 1 ? "s" : ""}
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
