3e88ba7cc099baa878ec7e841aed0b450526b366
heywowyou <pro@julesfontaine.com>
2025-03-27 15:49:21 +0100

    Limit last tweets length to 5

--- src/components/LastTweets.jsx
+++ src/components/LastTweets.jsx
@@ -1,39 +1,40 @@
-'use client';
-import { useEffect, useState } from 'react';
-import Tweet from './Tweet';
-
+"use client";
+import { useEffect, useState } from "react";
+import Tweet from "./Tweet";
 
 export default function LastTweets({ refresh }) {
-    const [tweets, setTweets] = useState([]);
+  const [tweets, setTweets] = useState([]);
 
-    const fetchTweets = () => {
-        fetch('http://localhost:3001/tweets')
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.result) {
-                    setTweets(data.tweets);
-                }
-            });
-    };
+  const fetchTweets = () => {
+    fetch("http://localhost:3001/tweets")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.result) {
+          setTweets(data.tweets);
+        }
+      });
+  };
 
-    useEffect(() => {
-        fetchTweets();
-    }, [refresh]);
+  useEffect(() => {
+    fetchTweets();
+  }, [refresh]);
 
-    return (
-        <div className="space-y-4">
-            {tweets.length === 0 ? (
-                <p className="text-gray-400">No tweets found.</p>
-            ) : (
-                tweets.map((tweet) => (
-                    <Tweet
-                        key={tweet._id}
-                        tweet={tweet}
-                        onDelete={fetchTweets}
-                        onLike={fetchTweets}
-                    />
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className="space-y-4">
+      {tweets.length === 0 ? (
+        <p className="text-gray-400">No tweets found.</p>
+      ) : (
+        tweets
+          .slice(0, 5)
+          .map((tweet) => (
+            <Tweet
+              key={tweet._id}
+              tweet={tweet}
+              onDelete={fetchTweets}
+              onLike={fetchTweets}
+            />
+          ))
+      )}
+    </div>
+  );
 }
